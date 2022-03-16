3d801c5027c9a0f898c51ed9b09a6a986d6860a3
jankes1006 <jankes1006@gmail.com>
2022-03-16 21:45:12 +0100

    Initial commit

---
+++ src/main/java/com/rubach/lesson_journal_server/LessonJournalServerApplication.java
@@ -0,0 +1,13 @@
+package com.rubach.lesson_journal_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonJournalServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonJournalServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rubach/lesson_journal_server/LessonJournalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubach.lesson_journal_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonJournalServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
